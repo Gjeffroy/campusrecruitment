673a9e9e221a9105840629c3d13787f34d4297c1
gjeffroy <guillaume.jeffory@mail.com>
2023-03-06 11:29:13 +0100

    update UI template

--- R/app_ui.R
+++ R/app_ui.R
@@ -13,47 +13,47 @@ app_ui <- function(request) {
       tags$head(
         tags$link(rel = "shortcut icon", href = "img/logo.png"),
         #-- biblio js ----
-        tags$link(rel="stylesheet", type = "text/css",
+        tags$link(rel = "stylesheet", type = "text/css",
                   href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"),
-        tags$link(rel="stylesheet", type = "text/css",
+        tags$link(rel = "stylesheet", type = "text/css",
                   href = "https://fonts.googleapis.com/css?family=Open+Sans|Source+Sans+Pro")
       ),
-      list(tags$head(HTML('<link rel="icon", href="img/logo.png",
-                        type="image/png" />'))),
-      div(style="padding: 1px 0px; width: '100%'",
-          titlePanel(
-            title="", windowTitle = "Campus Explorer"
-          ),
+      list(tags$head(
+        HTML('<link rel="icon", href="img/logo.png",
+                        type="image/png" />')
+      )),
+      div(
+        style = "padding: 1px 0px; width: '100%'; height:5px",
+        titlePanel(title = "", windowTitle = "Campus Explorer"),
 
       ),
 
       ##-- Header ----
-      navbarPage(title = div(img(src="www/logo.png",
-                                 height = "75px"), style = "padding-left:100px;"),
-                 id = "navbar",
-                 selected = "Home",
-                 theme = "www/custom.css",
-                 fluid = T,
-                 ##-- TABS ----
-                 tabPanel("Home",
-                          mod_home_ui("home_1")),
-                 tabPanel("Explore",
-                          mod_explore_ui("explore_1")),
-                 tabPanel("Profil",
-                          mod_profil_ui("profil_1")),
-                 tabPanel("Track",
-                          mod_track_ui("track_1")),
-                 tabPanel("Credit",
-                          mod_credit_ui("credit_1")),
+      navbarPage(
+        title = div(img(src = "www/logo.png",
+                        height = "60px"), style = "padding-left:100px;"),
+        id = "navbar",
+        selected = "Home",
+        theme = "www/custom.css",
+        fluid = T,
+        ##-- TABS ----
+        tabPanel("Home",
+                 mod_home_ui("home_1")),
+        tabPanel("Explore",
+                 mod_explore_ui("explore_1")),
+        tabPanel("Profil",
+                 mod_profil_ui("profil_1")),
+        tabPanel("Track",
+                 mod_track_ui("track_1")),
+        tabPanel("Credit",
+                 mod_credit_ui("credit_1")),
 
 
 
       ),
 
       ##-- Footer ----
-      div(
-        includeHTML("inst/app/www/footer.html")
-      )
+      div(includeHTML("inst/app/www/footer.html"))
     )
   )
 }
@@ -67,17 +67,13 @@ app_ui <- function(request) {
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
 golem_add_external_resources <- function() {
-  add_resource_path(
-    "www",
-    app_sys("app/www")
-  )
+  add_resource_path("www",
+                    app_sys("app/www"))
 
   tags$head(
     favicon(ext = 'png'),
-    bundle_resources(
-      path = app_sys("app/www"),
-      app_title = "campusrecruitment"
-    )
+    bundle_resources(path = app_sys("app/www"),
+                     app_title = "campusrecruitment")
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
   )
--- data-raw/colors.R
+++ data-raw/colors.R
@@ -1,5 +1,6 @@
 ## code to prepare `colors` dataset goes here
 
 box_colors <- c("#464d77","#f9db6d","#877666","#0b6e4f")
+
 usethis::use_data(box_colors, overwrite = T)
 
